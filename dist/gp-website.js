ead4a34b4a208010f5d478830b64e6813eb12785
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2018-09-14 19:10:33 +0100

    updating viewer and manually setting the viewer files

--- dist/gp-website.js
+++ dist/gp-website.js
@@ -4520,7 +4520,7 @@ var GenomePropertiesWebsite = (function () {
   }
   }).call(commonjsGlobal);
 
-  //# sourceMappingURL=showdown.js.map
+
   });
 
   var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
